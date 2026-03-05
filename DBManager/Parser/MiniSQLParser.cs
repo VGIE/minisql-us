8064320436236fc1199fe9193d3487add6f07c9b
Fabian <fmardale001@ikasle.ehu.eus>
2026-03-05 10:11:40 +0100

    #17 CreateTable RegularExpression done, CreateTable in MiniSQLParser class started

--- DBManager/Parser/MiniSQLParser.cs
+++ DBManager/Parser/MiniSQLParser.cs
@@ -1,4 +1,5 @@
 using DbManager.Parser;
+using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
@@ -17,13 +18,31 @@ namespace DbManager
 
             //Note: The parsing of CREATE TABLE should accept empty columns "()"
             //And then, an execution error should be given if a CreateTable without columns is executed
-            Match match = Regex.Match(miniSQLQuery, @"");
-            const string createTablePattern = null;
+            const string createTablePattern = @"CREATE TABLE(\w +) \((\w +\s(?:String | Int | Double)(?:,\w +\s(?:String | Int | Double)) *)*\)";
             
             const string updateTablePattern = null;
             
             const string deletePattern = null;
-            
+            Match match = Regex.Match(miniSQLQuery, createTablePattern);
+            if (match.Success) //Has there been a match?
+            {
+                String[] cols = match.Groups[2].Value.Split(',');
+                List<ColumnDefinition> columnas = new List<ColumnDefinition>();
+                foreach(String s in cols)
+                {
+                    String[] separados= s.Split(' ');
+                    String nombre = separados[0];
+                    String tipo = separados[1];
+                    //ColumnDefinition rcol= new ColumnDefinition()
+
+                }
+                //return new CreateTable(match.Groups[1].Value, );
+            }
+            else
+            {
+                Console.WriteLine("No matches found");
+            }
+
 
             //TODO DEADLINE 4
             const string createSecurityProfilePattern = null;
