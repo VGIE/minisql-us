9d3e726f22f99f0fd0a02afe3f3488d6ce061ae7
Julen <jcarasa002@ikasle.ehu.eus>
2026-03-05 13:16:45 +0100

    #15 Update if on MiniSQLParser Parse method implemented

--- DBManager/Parser/MiniSQLParser.cs
+++ DBManager/Parser/MiniSQLParser.cs
@@ -19,7 +19,7 @@ namespace DbManager
             //And then, an execution error should be given if a CreateTable without columns is executed
             const string createTablePattern = null;
             
-            const string updateTablePattern = null;
+            const string updateTablePattern = @"UPDATE\s+(\w+)\s+SET\s+(.*)\s+WHERE\s+(\w+)\s*(=|<|>)\s*(.*)";
             
             const string deletePattern = null;
             
@@ -43,6 +43,35 @@ namespace DbManager
             //For example, if the query is a "SELECT ...", there should be a match with selectPattern. We would create and return an instance of Select
             //initialized with the table name, the columns, and (possibly) an instance of Condition.
             //If there is no match, it means there is a syntax error. We will return null.
+            Match matchUpdate = Regex.Match(miniSQLQuery, updateTablePattern);
+            if (matchUpdate.Success)
+            {
+                string tableName = matchUpdate.Groups[1].Value;
+                string setString = matchUpdate.Groups[2].Value;
+                string condColumn = matchUpdate.Groups[3].Value;
+                string conditionOperator = matchUpdate.Groups[4].Value;
+                string conditionValue = matchUpdate.Groups[5].Value.Trim('\'');
+
+                List<SetValue> setValues = new List<SetValue>();
+                List<string> asignaciones = CommaSeparatedNames(setString);
+
+                foreach(string asignacion in asignaciones)
+                {
+                    string[] partes = asignacion.Split("=");
+                    if(partes.Length == 2)
+                    {
+                        string columna = partes[0].Trim();
+                        string valor = partes[1].Trim().Trim('\'');
+                        SetValue nuevo = new SetValue(columna, valor);
+                        setValues.Add(nuevo);
+                    }
+                }
+                Condition condition = new Condition(condColumn, conditionOperator, conditionValue);
+                Update consultaUpdate = new Update(tableName, setValues, condition);
+
+                return consultaUpdate;
+            }
+
 
             //TODO DEADLINE 4
             //Do the same for the security queries (CREATE SECURITY PROFILE, ...)
