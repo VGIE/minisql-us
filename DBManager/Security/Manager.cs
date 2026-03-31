2e462e5792749f5489e7d9fc88cabcfd4bf2ec34
Julen <jcarasa002@ikasle.ehu.eus>
2026-03-31 14:42:08 +0200

    #20 change to correct grant

--- DBManager/Security/Manager.cs
+++ DBManager/Security/Manager.cs
@@ -37,6 +37,12 @@ namespace DbManager.Security
         {
             //TODO DEADLINE 5: Add this privilege on this table to the profile with this name
             //If the profile or the table don't exist, do nothing
+            Profile perfilBuscado = ProfileByName(profileName);
+
+            if (perfilBuscado != null)
+            {
+                perfilBuscado.GrantPrivilege(table, privilege);
+            }
             
         }
 
@@ -44,12 +50,7 @@ namespace DbManager.Security
         {
             //TODO DEADLINE 5: Remove this privilege on this table to the profile with this name
             //If the profile or the table don't exist, do nothing
-            Profile perfilBuscado = ProfileByName(profileName);
-
-            if (perfilBuscado != null)
-            {
-                perfilBuscado.GrantPrivilege(table, privilege);
-            }
+            
 
         }
 
